827f40a2e74bf2b0f9604e970f42601d09784c19
Arthur-Ochieng <johnarthur016@gmail.com>
2019-09-03 15:01:56 +0300

    Added toggle effect on house moving section

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,14 @@
 
 //user interface logic
 $(document).ready(function(){
-    
+    $("div.house1").click(function() {
+        $("div.house2").toggle();
+        $("div.house1").toggle();
+      });
+      $("div.house2").click(function() {
+        $("div.house2").toggle();
+        $("div.house1").toggle();
+      });
 
 
     
